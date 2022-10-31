69570222a423e99eb4e94d29b872ee12b966bb2c
Ítalo de Rezende <italoeji@hotmail.com>
2022-10-31 04:02:58 -0300

    Adicionando favicon e title nas páginas

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import { Page } from "../components/page";
 
 export default function Dashboard() {
   return (
-    <Page>
-      <h1 className="text-white">hello</h1>
-    </Page>
+    <>
+      <Head>
+        <title>OmNaNFT - Dashboard</title>
+      </Head>
+      <Page>
+        <h1 className="text-white">hello</h1>
+      </Page>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import { Page } from "../components/page";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <Page homepage>
-      <div className="mx-auto flex h-full w-11/12 flex-col items-center justify-center">
-        <h1 className="text-5xl font-bold text-white">OmNaNFT</h1>
-        <Link href="/dashboard" passHref legacyBehavior>
-          <div className="mt-4 inline-block cursor-pointer rounded-sm bg-secondary p-4 font-semibold text-white transition duration-300 ease-in-out hover:bg-primary">
-            <a>Go to Dashboard ➡</a>
-          </div>
-        </Link>
-      </div>
-    </Page>
+    <>
+      <Head>
+        <title>OmNaNFT - Home</title>
+      </Head>
+      <Page homepage>
+        <div className="mx-auto flex h-full w-11/12 flex-col items-center justify-center">
+          <h1 className="text-5xl font-bold text-white">OmNaNFT</h1>
+          <Link href="/dashboard" passHref legacyBehavior>
+            <div className="mt-4 inline-block cursor-pointer rounded-sm bg-secondary p-4 font-semibold text-white transition duration-300 ease-in-out hover:bg-primary">
+              <a>Go to Dashboard ➡</a>
+            </div>
+          </Link>
+        </div>
+      </Page>
+    </>
   );
 }
