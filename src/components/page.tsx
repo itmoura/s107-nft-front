a60047199bc0cd920b4a88aeaaf1b70c9f29d3a0
Ítalo de Rezende <italoeji@hotmail.com>
2022-11-30 10:48:25 -0300

    Ajustes no tamanho da página

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -13,9 +13,9 @@ export const Page = ({ children, homepage = false }: PageProps) => {
       <Navbar />
       {!homepage && <Sidebar />}
       <div
-        className={`fixed top-[73px] overflow-auto ${
-          !homepage && "left-60"
-        } h-screen w-screen bg-background`}
+        className={`fixed top-[73px] overflow-auto ${!homepage && "left-60"} ${
+          !homepage ? "w-[calc(100%-15rem)]" : "w-screen"
+        } h-[calc(100%-73px)]  bg-background`}
       >
         {children}
       </div>
