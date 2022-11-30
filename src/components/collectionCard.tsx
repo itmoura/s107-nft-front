a952ed2d5b3e8211cae0f4e7e0bcec51d720414f
Ítalo de Rezende <italoeji@hotmail.com>
2022-11-30 10:46:12 -0300

    Adicionando componente card de Coleção

---
+++ src/components/collectionCard.tsx
@@ -0,0 +1,10 @@
+export const CollectionCard = () => {
+  return (
+    <div className="flex min-w-[250px] flex-col rounded-tl-lg rounded-br-lg bg-secondary p-4">
+      <div className="h-[160px] rounded-tl-lg rounded-br-lg bg-primary"></div>
+      <div className="flex flex-1 flex-col py-4">
+        <span className="font-bold text-white">Coleção braba</span>
+      </div>
+    </div>
+  );
+};
