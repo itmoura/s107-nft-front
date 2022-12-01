3af222db84ccd2219f3454e644ef308cf8b1dcfa
Ítalo de Rezende <italoeji@hotmail.com>
2022-12-01 20:46:08 -0300

    Adicionando providers e toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+import { Providers } from "../providers";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Providers>
+      <Component {...pageProps} />
+      <Script
+        src="https://widget.cloudinary.com/v2.0/global/all.js"
+        type="text/javascript"
+      />
+      <ToastContainer position="top-center" />
+    </Providers>
+  );
 }
